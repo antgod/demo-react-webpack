05f1b42db2cf235bb79a7677e0e9be8a5a66ebce
李宏吉 <m18513622209@163.com>
2017-04-02 20:15:08 +0800

    add

---
+++ doc/test.js
@@ -0,0 +1,8 @@
+const fs = require('fs')
+
+const a= fs.readFileSync('1.初入React世界.md', 'utf8')
+
+const b= a.replace(/pre\>[\s\S]*(\n)[\s\S]*\<\/pre/, function(word){
+  return word.replace(/\n\n/g, '\n')
+})
+console.log(b)
